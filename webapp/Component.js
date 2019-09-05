3d7dd0d1ef904bd733400967025205d3b0109153
Bao Gao <bao.gao@sap.com>
2019-09-05 08:09:08 +0000

    index.html

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("ns_suppliers_list.zsuppliers_list.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
